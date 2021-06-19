9341bbc709eddb0fe6b0b59ede89294f110fca1d
srivanireddy <srivanireddy1432@gmail.com>
2021-06-19 13:37:48 +0530

    CRUD operations using Promises and Async & Await

--- APIS/user-api.js
+++ APIS/user-api.js
@@ -25,7 +25,7 @@ mc.connect(databaseUrl,{useNewUrlParser:true,useUnifiedTopology:true},(err,clien
     }
 })
 
-//http://localhost:3000/user/getusers
+/*http://localhost:3000/user/getusers
 userApi.get("/getusers",(req,res,next)=>{
 
     //read docs from user collection
@@ -44,7 +44,30 @@ userApi.get("/getusers",(req,res,next)=>{
 
 })
 
-//http://localhost:3000/user/getuser/<username>
+//http://localhost:3000/user/getusers
+userApi.get("/getusers",(req,res,next)=>{
+
+    //read docs from user collection
+    userCollectionObj.find().toArray().
+        then(usersList=>{res.send({message:usersList})}).
+        catch(err=>{
+            console.log("err in getting usersList",err)
+            res.send({message:err.message});
+        })
+
+
+})*/
+
+//http://localhost:3000/user/getusers
+userApi.get("/getusers", async (req,res,next)=>{
+
+    //read docs from user collection
+    let usersList = await userCollectionObj.find().toArray()
+    res.send({message:usersList})
+
+})
+
+/*http://localhost:3000/user/getuser/<username>
 userApi.get("/getuser/:username",(req,res,next)=>{
 
     //get username from urlparamas
@@ -73,8 +96,44 @@ userApi.get("/getuser/:username",(req,res,next)=>{
 
 })
 
+//http://localhost:3000/user/getuser/<username>
+userApi.get("/getuser/:username",(req,res,next)=>{
 
-http://localhost:3000/user/createuser
+    //get username from urlparamas
+    let un=req.params.username
+
+    //search for user
+    userCollectionObj.findOne({username:{$eq:un}}).
+        then(userObj=>{
+            if(userObj==null)
+                res.send({message:"user not found"})
+            else
+                res.send({message:userObj});
+        }).
+        catch(err=>{
+            console.log("err in getting user by username",err)
+            res.send({message:err.message});
+        })
+
+})*/
+
+//http://localhost:3000/user/getuser/<username>
+userApi.get("/getuser/:username", async (req,res,next)=>{
+
+    //get username from urlparamas
+    let un=req.params.username
+
+    //search for user
+    let userObj = await userCollectionObj.findOne({username:{$eq:un}})
+    if(userObj==null)
+        res.send({message:"user not found"})
+    else
+        res.send({message:userObj});
+
+})
+
+
+/*http://localhost:3000/user/createuser
 userApi.post("/createuser",(req,res,next)=>{
 
     //get user Obj
@@ -113,8 +172,57 @@ userApi.post("/createuser",(req,res,next)=>{
 
 })
 
+//http://localhost:3000/user/createuser
+userApi.post("/createuser",(req,res,next)=>{
 
-//http://localhost:3000/user/updateuser/<username>
+    //get user Obj
+    let newUser=req.body
+
+    //check if user is already existed
+    userCollectionObj.findOne({username:newUser.username}).
+        then(userObj=>{
+            //if user not existed create new user
+            if(userObj==null){
+                userCollectionObj.insertOne(newUser)
+                console.log("User added successfully")
+                res.send({message:"User added successfully"})
+            }
+            //if user existed already
+            else{
+                console.log("User existed already")
+                res.send({message:"User existed already"})
+            }
+        }).
+        catch(err=>{
+            console.log("error in creating user ",err)
+            res.send({message:err.message})
+        })
+
+})*/
+
+//http://localhost:3000/user/createuser
+userApi.post("/createuser", async (req,res,next)=>{
+
+    //get user Obj
+    let newUser=req.body
+
+    //check if user is already existed
+    let userObj = await userCollectionObj.findOne({username:newUser.username})
+    //if user not existed create new user
+    if(userObj==null){
+        userCollectionObj.insertOne(newUser)
+        console.log("User added successfully")
+        res.send({message:"User added successfully"})
+    }
+    //if user existed already
+    else{
+        console.log("User existed already")
+        res.send({message:"User existed already"})
+    }
+
+})
+
+/*http://localhost:3000/user/updateuser/<username>
 userApi.put("/updateuser/:username",(req,res,next)=>{
 
     //get user from url params
@@ -123,10 +231,9 @@ userApi.put("/updateuser/:username",(req,res,next)=>{
     //update user
     userCollectionObj.updateMany({username:newUser.username},{
             $set:{
-                /*
-                email:newUser.email,
-                city:newUser.city,
-                age:newUser.age*/
+                //email:newUser.email,
+                //city:newUser.city,
+                //age:newUser.age
                 ...newUser
             }
             },(err,success)=>{
@@ -140,13 +247,42 @@ userApi.put("/updateuser/:username",(req,res,next)=>{
                     res.send({message:"User data updated"})
                 }
     })
+})
 
 
+//http://localhost:3000/user/updateuser/<username>
+userApi.put("/updateuser/:username",(req,res,next)=>{
+
+    //get user from url params
+    let newUser=req.body
+
+    //update user
+    userCollectionObj.updateMany({username:newUser.username},{$set:{ ...newUser }}).
+        then(success=>{
+            console.log("User data updated")
+            res.send({message:"User data updated"})
+        }). 
+        catch(err=>{
+            console.log("error in updating user ",err)
+            res.send({message:err.message})
+        })
+})*/
+
+//http://localhost:3000/user/updateuser/<username>
+userApi.put("/updateuser/:username", async (req,res,next)=>{
+
+    //get new user
+    let newUser = req.body;
+
+    //update
+    await userCollectionObj.updateMany({username:newUser.username},{$set:{...newUser}})
+    console.log("User data updated")
+    res.send({message:"User data updated"})
 
 })
 
 
-// http://localhost:3000/user/deleteuser/arun
+/* http://localhost:3000/user/deleteuser/arun
 userApi.delete('/deleteuser/:username',(req,res,next)=>{
 
     //get username from url paramas
@@ -165,7 +301,35 @@ userApi.delete('/deleteuser/:username',(req,res,next)=>{
     })
 })
 
+// http://localhost:3000/user/deleteuser/arun
+userApi.delete('/deleteuser/:username',(req,res,next)=>{
 
+    //get username from url paramas
+    let userObj=req.params.username
+
+    //delete user
+    userCollectionObj.deleteMany({username:userObj}).
+        then(success=>{
+            console.log("User Deleted")
+            res.send({message:"User Deleted"})
+        }).
+        catch(err=>{
+            console.log("error in deleting user ",err)
+            res.send({message:err.message})
+        })
+})*/
+
+// http://localhost:3000/user/deleteuser/<username>
+userApi.delete('/deleteuser/:username', async (req,res,next)=>{
+
+    //get username from url params
+    let userObj=req.params.username
+
+    //deleteuser
+    await userCollectionObj.deleteMany({username:userObj})
+    console.log("User Deleted")
+    res.send({message:"User Deleted"})
+})
 
 
 
